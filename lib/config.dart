ea7eb70e28756cabeea35d192bb673b3da1da7e1
YokaiLab <dev@yokailab.dev>
2026-03-25 16:41:24 +0900

    feat: connect to production backend

--- lib/config.dart
+++ lib/config.dart
@@ -1,8 +1,8 @@
 class AppConfig {
-  // Backend server URL - change this to your server address
+  // Backend server URL
   // For emulator: http://10.0.2.2:3000
-  // For real device on same network: http://YOUR_PC_IP:3000
-  static const String backendUrl = 'http://10.0.2.2:3000';
+  // For real device: http://YOUR_SERVER_IP:3000
+  static const String backendUrl = 'http://159.194.206.53:3000';
 
   // Polling interval in seconds
   static const int pollingIntervalSeconds = 3;
